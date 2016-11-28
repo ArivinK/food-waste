31a72cffc965fb1828c37adfc3ad4a834a252e50
Uzochukwu Odozi <uzo.odozi@gmail.com>
2016-11-28 17:05:40 +0100

    Created the signup and login modals with forms

--- controllers/user.js
+++ controllers/user.js
@@ -7,6 +7,10 @@ router.get('/', (req, res) => {
    res.render('index.ejs'); 
 });
 
+router.get('/user/add-info', (req, res) => {
+   res.render('user/add-info.ejs'); 
+});
+
 
 module.exports = router;
 
